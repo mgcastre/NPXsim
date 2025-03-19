9946e13e342353b97f1478d56df2c48e0f0bb98f
M. G. Castrellon <g.castrellonromero@un-ihe.org>
2025-03-19 18:24:00 +0100

    Created method to calculate basin operational levels

--- lock_model/neo_panamax_lock.py
+++ lock_model/neo_panamax_lock.py
@@ -21,7 +21,7 @@ class NeoPanamaxLock(ThreeStepsLock):
                          lock_head_sills=lock_head_sills)
 
         # Initialize water saving basin objects
-        self.basins = {'LC': {}, 'MC': {}, 'LC': {}}
+        self.basins = {'LC': {}, 'MC': {}, 'UC': {}}
         for cham in self.basins.keys():
             for basin in ['Top', 'Int', 'Btm']:
                 self.basins[cham][basin] = WaterSavingBasin(
@@ -29,3 +29,21 @@ class NeoPanamaxLock(ThreeStepsLock):
                     z_bottom=wsb_bottom_elevs[cham][basin],
                     S0=None, H0=None
                 )
+    
+    @staticmethod
+    def calc_wsb_operational_levels(chamber_levels):
+        wsb_levels = {}
+        fractions = {'Top': (3, 4), 'Int': (2, 3), 'Btm': (1, 2)}
+        for chamber, (c_low, c_high) in chamber_levels.items():
+            wsb_levels[chamber] = {}
+            for basin, (n_low, n_high) in fractions.items():
+                level_low = c_low + (n_low/5)*(c_high - c_low)
+                level_high = c_low + (n_high/5)*(c_high - c_low)
+                wsb_levels[chamber][basin] = (level_low, level_high)
+        return wsb_levels
+    
+    def calc_operational_levels(self, H_lake, H_ocean):
+        cham_op_levels = super().calc_operational_levels(H_lake, H_ocean)
+        wsb_op_levels = self.calc_wsb_operational_levels(cham_op_levels)
+        return cham_op_levels, wsb_op_levels
+    
