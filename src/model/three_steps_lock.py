500d26bec1084f77a69c1e62ab54bd963f251663
M. G. Castrellon <g.castrellonromero@un-ihe.org>
2025-06-13 17:43:24 +0200

    Updated methods to organize outputs

--- src/model/three_steps_lock.py
+++ src/model/three_steps_lock.py
@@ -5,8 +5,8 @@
 import numpy as np
 import pandas as pd
 from datetime import datetime
-from classes.lock_elements import *
-from classes.custom_exceptions import *
+from model.lock_elements import *
+from model.custom_exceptions import *
 import utilities.hydrodynamics as hd
 
 # Define class
@@ -33,10 +33,6 @@ class ThreeStepsLock:
             'LH3': ['LC', 'MC'], 
             'LH4': ['LC']
         }
-        # Initialize dict to store salt mass load to the lake
-        self.salt_mass_load = {'TS': [], 'DC': [], 'VD': []}
-        # Initialize dict to store freshwater consumed per lockage
-        self.freshwater_consumed = {'TS': [], 'ConsMMC': []}
     
     def calc_cham_operational_levels(self, H_lake, H_ocean):
         # Extract chamber areas
@@ -90,6 +86,10 @@ class ThreeStepsLock:
         self.T = water_temperature
         # Add master initial operation start time
         self.operation_start_dt = operation_start_dt
+        # Initialize dicts to store freshwater consumption and salt mass load
+        self.salt_mass_load = {'Num': [], 'TS': [], 'DC': [], 'VD': []}
+        self.freshwater_use = {'Num': [], 'TS': [], 'ConsMMC': [], 
+                               'OP_Type': [], 'Start_LUC': [], 'End_LUC': []}
     
     def extract_properties(self, cham):
         W = self.chambers[cham].width
@@ -193,16 +193,29 @@ class ThreeStepsLock:
             m_vd = -1*self.V_ship*c1
         elif direction == 'down':
             m_vd = self.V_ship*c2
-        # Recording salt mass load in UK Tonnes
+        # Recording salt mass load in kilograms
         self.salt_mass_load['TS'].append(ts)
-        self.salt_mass_load['DC'].append(m_dc/1000)
-        self.salt_mass_load['VD'].append(m_vd/1000)
+        self.salt_mass_load['DC'].append(m_dc)
+        self.salt_mass_load['VD'].append(m_vd)
+        self.salt_mass_load['Num'].append(self.Num)
     
-    def record_freshwater_consumed(self, start_luc, end_luc, ts):
-        # Recording amount of freshwater used in million cubic meters
+    def record_freshwater_use(self, start_luc, end_luc, ts, op_type):
+        # Calculate amount of freshwater used in million cubic meters
         vol = (end_luc - start_luc)*self.chambers['UC'].area
-        self.freshwater_consumed['ConsMMC'].append(vol/1e6)
-        self.freshwater_consumed['TS'].append(ts)
+        
+        # Record the freshwater consumption
+        self.freshwater_use['Start_LUC'].append(start_luc)
+        self.freshwater_use['End_LUC'].append(end_luc)
+        self.freshwater_use['ConsMMC'].append(vol)
+        self.freshwater_use['OP_Type'].append(op_type)
+        self.freshwater_use['TS'].append(ts)
+        
+        if op_type == 'TA':
+            lockage_id = self.Num + 1
+        else:
+            lockage_id = self.Num
+        
+        self.freshwater_use['Num'].append(lockage_id)
     
     def exchange_with_boundary(self, S_lake=None, S_ocean=None):
         if S_lake is not None:
@@ -234,11 +247,12 @@ class ThreeStepsLock:
             except IndexError:
                 break
             if this_transit['Direction'] != next_transit['Direction']:
-                ts = self.calc_elapsed_minutes(next_transit['TS_LocksReady']) - 30
+                ts = self.calc_elapsed_minutes(next_transit['TS_LockageStarts']) - 30
                 self.turnaround(boundary_conditions[i+1], next_transit['Direction'], tinit=ts)
     
     def transit(self, operation_params, boundary_conditions):
-        # Extract volume of the ship transiting the lock
+        # Extract transit parameters
+        self.Num = operation_params['Num']
         self.V_ship = operation_params['V_ship']
         # Extract boundary conditions
         S_ocean = boundary_conditions['S_ocean']
@@ -283,7 +297,7 @@ class ThreeStepsLock:
         start_luc = self.chambers['UC'].get_current_level()
         time_stamp = time_stamp + self.eqTime['UC'] # minutes to fill chamber
         self.chambers['UC'].fill_chamber(H_final=H_lake, S_lift=S_lake, ts=time_stamp)
-        self.record_freshwater_consumed(start_luc=start_luc, end_luc=H_lake, ts=time_stamp)
+        self.record_freshwater_use(start_luc=start_luc, end_luc=H_lake, ts=time_stamp, op_type='TR')
         ## 6) Gates at LH1 open, salt mass enters the lake and ship leaves the lock
         t_transit = self.tGateOpen['LH1'] # minutes
         time_stamp = time_stamp + t_transit # minutes
@@ -297,7 +311,7 @@ class ThreeStepsLock:
         if self.chambers['UC'].get_current_level() < H_lake:
             start_luc = self.chambers['UC'].get_current_level()
             self.chambers['UC'].fill_chamber(H_final=H_lake, S_lift=S_lake, ts=initial_time_stamp)
-            self.record_freshwater_consumed(start_luc=start_luc, end_luc=H_lake, ts=initial_time_stamp)
+            self.record_freshwater_use(start_luc=start_luc, end_luc=H_lake, ts=initial_time_stamp, op_type='TR')
         ## 2) Gates at LH1 open, salt mass enters the lake and ship enters the lock
         t_transit = self.tGateOpen['LH1'] # minutes
         time_stamp = initial_time_stamp + t_transit # minutes
@@ -319,6 +333,12 @@ class ThreeStepsLock:
         self.chambers['LC'].ship_leaves(V_rhs=V_ex_ocean, S_rhs=S_ocean, ts=time_stamp)
     
     def turnaround(self, boundary_conditions, new_direction, tinit):
+        # Record current conditions before initiating turnaround
+        self.chambers['MC'].record_current_status(ts=tinit)
+        if new_direction == 'up':
+            self.chambers['UC'].record_current_status(ts=tinit)
+        elif new_direction == 'down':
+            self.chambers['LC'].record_current_status(ts=tinit)
         # Extract boundary conditions
         H_ocean = boundary_conditions['H_ocean']
         S_lake = boundary_conditions['S_lake']
@@ -329,6 +349,8 @@ class ThreeStepsLock:
         if new_direction == 'up':
             ## 1. Fill UC to the level of the Lake
             ts = tinit + 10 # minutes
+            H_uc_current = self.chambers['UC'].get_current_level()
+            self.record_freshwater_use(start_luc=H_uc_current, end_luc=H_lake, ts=ts, op_type='TA')
             self.chambers['UC'].fill_chamber(H_final=H_lake, S_lift=S_lake, ts=ts)
             ## 2. Drain UC and fill MC to the top operating level of MC
             ts = ts + 10 # minutes
@@ -338,8 +360,10 @@ class ThreeStepsLock:
             self.chambers['MC'].fill_chamber(H_final=Hf_mc, S_lift=S_uc, ts=ts)
             ## 3. Fill UC again to the level of the ocean
             ts = ts + 10 # minutes
+            H_uc_current = self.chambers['UC'].get_current_level()
+            self.record_freshwater_use(start_luc=H_uc_current, end_luc=H_lake, ts=ts, op_type='TA')
             self.chambers['UC'].fill_chamber(H_final=H_lake, S_lift=S_lake, ts=ts)
-            ## 4. Drain LC to the level of the ocean
+            ## 4. Drain LC to the level of the ocean (if needed)
             if self.chambers['LC'].get_current_level() > H_ocean:
                 self.chambers['LC'].drain_chamber(H_final=H_ocean, ts=ts)
         # B) From uplockage to downlockage:
@@ -356,28 +380,108 @@ class ThreeStepsLock:
             ## 3. Drain LC again to the level of the ocean
             ts = ts + 10 # minutes
             self.chambers['LC'].drain_chamber(H_final=H_ocean, ts=ts)
-            ## 4. Fill UC to the level of the lake
+            ## 4. Fill UC to the level of the lake (if needed)
             if self.chambers['UC'].get_current_level() < H_lake:
+                H_uc_current = self.chambers['UC'].get_current_level()
+                self.record_freshwater_use(start_luc=H_uc_current, end_luc=H_lake, ts=ts, op_type='TA')
                 self.chambers['UC'].fill_chamber(H_final=H_lake, S_lift=S_lake, ts=ts)
     
-    def get_salt_load(self, Units='ton', Dictionary=True):
+    def get_salt_load(self, units='ton'):
         """
-        Returns the salt mass load etering the lake from the upper chamber.
-        By default it returns a dictionary with the keys 'DC', 'VD', and 'Total'.
-        If Dictionary is set to False, it returns the values in a tuple.
-        By default, it returns the mass in tonnes, but it can also be returned in 
-        kilogram by setting Units to 'kg'.
-        """	
-        sm_dc = np.array(self.salt_mass_load['DC'])
-        sm_vd = np.array(self.salt_mass_load['VD'])
-        if Units == 'ton':
-            sm_dc = sm_dc/1000
-            sm_vd = sm_vd/1000
-        m_total = sm_dc + sm_vd
-        if Dictionary:
-            return {'DC': sm_dc, 'VD': sm_vd, 'Total': m_total}
+        Calculate and return the salt mass load entering the lake from the upper chamber.
+        Parameters
+        ----------
+        Units : str, optional
+            Unit of mass for the output values. Accepts 'ton' for tonnes (default) or 'kg' for kilograms.
+        Returns
+        -------
+        pandas.DataFrame
+            DataFrame containing the salt mass load with columns:
+                - 'TS': Timestamp or time step identifier.
+                - 'DC': Salt load from the DC source (in specified units).
+                - 'VD': Salt load from the VD source (in specified units).
+                - 'Total_Salt_Load': Combined salt load from DC and VD (in specified units).
+        Notes
+        -----
+        If `Units` is set to 'ton', the values for 'DC' and 'VD' are converted from kilograms to tonnes.
+        The 'Total_Salt_Load' column is computed as the sum of 'DC' and 'VD' for each row.
+        """
+        df = pd.DataFrame(self.salt_mass_load)
+        df = df.sort_values(by='TS', ascending=True)
+        
+        if units == 'ton':
+            for col in ['DC', 'VD']:
+                df[col] = df[col]/1000
+        
+        df['Total_Salt_Load'] = df['DC'] + df['VD']
+
+        return df
+    
+    def get_freshwater_use(self, units='hm3'):
+        """
+        Returns the amount of freshwater use in the simulation.
+        Parameters
+        ----------
+        Units : str, optional
+            The units for the returned freshwater consumption values.
+            Supported values:
+                - 'hm3': Returns consumption in hectometers cubed (default).
+                - Any other value: Returns consumption in the original units (cubic meters).
+        Returns
+        -------
+        pandas.DataFrame
+            DataFrame containing freshwater consumption data. If Units is 'hm3',
+            the 'ConsMMC' column is converted from cubic meters to hectometers cubed.
+        """
+        df = pd.DataFrame(self.freshwater_use)
+        df = df.sort_values(by='TS', ascending=True)
+        df['Delta_LUC'] = df['End_LUC'] - df['Start_LUC']
+
+        if units == 'hm3':
+            df['ConsMMC'] = df['ConsMMC']/1e6
+        
+        return df
+    
+    def get_operation_outputs(self):
+        """
+        Generates a DataFrame summarizing the operational outputs for the lake system.
+        This method computes and merges the freshwater use and the salt mass load 
+        (in tons) entering the lake from the upper chamber. The resulting DataFrame is 
+        indexed by the lockage number or ID, with columns for both freshwater use 
+        and salt load.
+        Returns:
+            pd.DataFrame: A DataFrame containing columns for freshwater use and 
+            salt mass load, aligned by lockage number/ID.
+        """
+        water_use_raw = self.get_freshwater_use(units='hm3')
+        salt_load_raw = self.get_salt_load(units='ton')
+        
+        ## Pivot the water use DataFrame
+        water_use = pd.pivot_table(
+            water_use_raw, values='ConsMMC', index='Num', 
+            columns='OP_Type', aggfunc='sum')
+
+        ## Format water use DataFrame
+        if 'TA' in water_use.columns:
+            water_use['TA'] = water_use['TA'].fillna(0)
+            water_use.columns = ['TA_Water_Use', 'TR_Water_Use']
         else:
-            return sm_dc, sm_vd, m_total
+            water_use['TA_Water_Use'] = 0
+            water_use.rename(columns={'TR': 'TR_Water_Use'}, inplace=True)
+        
+        water_use['Total_Water_Use'] = water_use.sum(axis=1)
+        water_use.reset_index(inplace=True)
+        water_use.columns.name = None
+
+        ## Format salt load DataFrame
+        salt_load = salt_load_raw.copy()
+        salt_load.drop(columns='TS', inplace=True)
+        salt_load.rename(columns={'DC': 'DC_Salt_Load', 'VD': 'VD_Salt_Load'}, inplace=True)
+
+        ## Merge DataFrame on Num column
+        df = pd.merge(water_use, salt_load, on='Num', how='outer')
+
+        return df
     
     def make_results_df(self):
         """Returns a dataframe with the raw results of the lock model."""
@@ -394,6 +498,12 @@ class ThreeStepsLock:
         # Concatenate the list of dataframes and return the results
         results = pd.concat(list_of_results)
         return results
+    
+    def convert_col_to_datetime(self, df, col):
+        initial_time = self.operation_start_dt
+        df[col] = pd.to_timedelta(df[col], unit='min')
+        df['Date_Time'] = pd.to_datetime(initial_time) + df[col]
+        return df
 
     def get_results(self, variable, dt_index, ffill):
         """
@@ -408,9 +518,7 @@ class ThreeStepsLock:
         df = df.pivot(index='Time', columns='Chamber', values=variable)
         # Add a datetime column as index
         df.reset_index(inplace=True)
-        initial_time = self.operation_start_dt
-        df['Time'] = pd.to_timedelta(df['Time'], unit='min')
-        df['Date_Time'] = pd.to_datetime(initial_time) + df['Time']
+        df = self.convert_col_to_datetime(df=df, col='Time')
         if dt_index:
             df.set_index('Date_Time', inplace=True)
             df.drop(columns='Time', inplace=True)
