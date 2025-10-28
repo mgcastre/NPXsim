aed1651015a59497a4534b5bfea063a418820997
M. G. Castrellon <mariag.castrellon@gmail.com>
2025-10-28 21:45:39 +0100

    Turnarounds for all lockages do not use basins

--- src/npxsim/model/neo_panamax_lock.py
+++ src/npxsim/model/neo_panamax_lock.py
@@ -263,14 +263,19 @@ class NeoPanamaxLock(ThreeStepsLock):
                 break
             if this_transit['Direction'] != next_transit['Direction']:
                 ts = self.calc_elapsed_minutes(next_transit['TS_LockageStarts'])
-                if this_transit['WSB_Use_Flag']:
-                    tinit = ts - 40  # Start turnaround operation 40 minutes before next transit
-                    logger.info(f'[{self.ts_to_datetime(tinit)}] - TURNAROUND WITH BASINS STARTS')
-                    self.turnaround(boundary_conditions[i+1], next_transit['Direction'], tinit=tinit)
-                else:
-                    tinit = ts - 30 # Start turnaround operation 30 minutes before next transit
-                    logger.info(f'[{self.ts_to_datetime(tinit)}] - TURNAROUND WITHOUT BASINS STARTS')
-                    super().turnaround(boundary_conditions[i+1], next_transit['Direction'], tinit=tinit)
+                tinit = ts - 30 # Start turnaround operation 30 minutes before next transit
+                logger.info(f'[{self.ts_to_datetime(tinit)}] - TURNAROUND WITHOUT BASINS STARTS')
+                super().turnaround(boundary_conditions[i+1], next_transit['Direction'], tinit=tinit)
+
+                # if this_transit['WSB_Use_Flag']:
+                #     tinit = ts - 40  # Start turnaround operation 40 minutes before next transit
+                #     logger.info(f'[{self.ts_to_datetime(tinit)}] - TURNAROUND WITH BASINS STARTS')
+                #     self.turnaround(boundary_conditions[i+1], next_transit['Direction'], tinit=tinit)
+                # else:
+                #     tinit = ts - 30 # Start turnaround operation 30 minutes before next transit
+                #     logger.info(f'[{self.ts_to_datetime(tinit)}] - TURNAROUND WITHOUT BASINS STARTS')
+                #     super().turnaround(boundary_conditions[i+1], next_transit['Direction'], tinit=tinit)
+
         logger.info('NORMAL TERMINATION OF LOCK OPERATIONS')
     
     def transit(self, operation_params, boundary_conditions):
